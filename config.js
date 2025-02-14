97ffd4f04a9479d4b5f2772ed477b097ab7edaa5
Brandon Jang <brandonjang90@gmail.com>
2025-02-13 22:46:58 -0800

    update

--- config.js
+++ config.js
@@ -10,11 +10,18 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
+// function getDatabaseUri() {
+//   return (process.env.NODE_ENV === "test")
+//       ? "jobly_test"
+//       : process.env.DATABASE_URL || "jobly";
+// }
 function getDatabaseUri() {
-  return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+  if (process.env.NODE_ENV === "test") {
+    return "postgresql://localhost:5432/jobly_test";
+  }
+  return process.env.DATABASE_URL || "postgresql://localhost:5432/jobly";
 }
+
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
 //
